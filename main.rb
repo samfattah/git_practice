016e08593bb24c8cf3fd80242492270c90f3b2e7
Sam Abdelfattah <samfatth@gmail.com>
2017-08-16 11:34:33 -0600

    completed git command help program

--- main.rb
+++ main.rb
@@ -1,5 +1,24 @@
-puts 'Learning Git'
-first_name = 'Sam'
-last_name = 'Fattah'
+def puts_git(cmd)
+    puts `git #{cmd} -h`
+end
 
-puts "Hello #{first_name} #{last_name}"
+def menu
+    puts '1: Enter a Git Command'
+    puts '2: Exit'
+    choice = gets.to_i
+    case choice
+        when 1
+            puts "Enter a Git Command"
+            puts_git(gets.strip)
+        when 2
+            puts "Thanks for using the git command"
+            exit(0)
+        else
+            puts "Invalid Choice!"
+            menu
+    end
+    menu
+end
+
+menu
+
